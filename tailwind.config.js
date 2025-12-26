0fba24cf16b25914eeca610de5e0a683abe0b45a
Gabriel James <gabbyjames@yahoo.com>
2025-12-26 11:28:53 +0100

    Enable Tailwind class-based dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // Use class-based dark mode so toggling `document.documentElement.classList` works
+  darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
